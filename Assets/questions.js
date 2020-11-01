6898c67ceb7cd6a48869bb2ba73165917a84b8c6
Max Lechner <mkonlech@gmail.com>
2020-10-31 23:36:05 -0700

    add in new code

---
+++ Assets/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+    {
+        question: 'Which term describes the skeletal layout of a Web page, without colors or graphics?',
+        answer1: 'Mind map',
+        answer2: 'Use Case',
+        answer3: 'Agile Card',
+        answer4: 'wireframe',
+        correctAnswer: 4,
+    },
+    {
+        question: 'What is the language used for describing the look and formatting of a document written in markup language?',
+        answer1: 'CSS',
+        answer2: 'HTML',
+        answer3: 'JSON',
+        answer4: 'REACT',
+        correctAnswer: 1,
+    },
+    {
+        question: 'What does HTML stand for?',
+        answer1: 'Hotkey Tool Markup Linter',
+        answer2: 'Hyper Text Markup Language',
+        answer3: 'Harriet Tanner Miriam Louise',
+        answer4: 'Half Turn Miter Lift',
+        correctAnswer: 2,
+    },
+    {
+        question: 'What tag element will display a line across the screen?',
+        answer1: 'a',
+        answer2: 'card',
+        answer3: 'br',
+        answer4: 'hr',
+        correctAnswer: 4,
+    },
+    {
+        question: 'Which are used with a tag to modify its function?',
+        answer1: 'functions',
+        answer2: 'css',
+        answer3: 'hotkeys',
+        answer4: 'attributes',
+        correctAnswer: 4,
+    },
+
+    
+];
--- Assets/script.js
+++ Assets/script.js
@@ -1,257 +1,141 @@
-var buttonStart = document.getElementById("start-game");
-var timeLeft = document.getElementById("time-left");
-var introBlock = document.getElementById("intro-block");
-var questionSection = document.getElementById("questionModal");
-var answers = document.getElementById("answers");
-var questionEl = document.getElementById("question");
-var correctOrWrongEl = document.getElementById("correctOrWrongAnswer");
+// start screen
+const startBtnEl = document.getElementById('startBtn');
+const startScreenEl = document.getElementById('startScreen');
+const viewHighscoresEl = document.getElementById('viewHighscores')
 
-var scoreInfo = document.getElementById("scoreInfo");
-var submitBtn = document.getElementById("intials-button");
-var intialsEl = document.getElementById("intials");
-var totalScore = document.getElementById("score");
+// questions
+const questionEl = document.getElementById('question');
+const questionContainerEl = document.getElementById('questionContainer');
+const answers = Array.from(document.querySelectorAll('.answerBtn'));
+const correctEl = document.getElementById('correctSign');
+const wrongEl = document.getElementById('wrongSign');
+const correctWrongSectionEl = document.getElementById('correctAndWrong');
 
-var backButtonEl = document.getElementById("go-back");
-var clearButton = document.getElementById("clear");
-var listHighscores = document.getElementById("player-list");
+let randomQuestion;
+let questionIndex;
+let currentQuestion;
 
-var interval;
-var score;
-var totalSeconds = 120;
-var currentQuestion = 0;
+// game end
+const gameOverEl = document.getElementById('gameOverScreen');
+const submitBtnEl = document.getElementById('submitBtn');
+const retakeBtnEl = document.getElementById('retakeBtn');
+const userIdEL = document.getElementById('#userInitials');
 
-var questions = [
-    questionOne = {
-        q : "Whats the most common way to create a variable?",
-        a: ["var","int","float","string"],
-        rightAnswer : 0
-    },
-    questionTwo = {
-        q : "What key word is used to define a function?",
-        a: ["module","var","float","function"],
-        rightAnswer : 3
-    },
-    questionThree = {
-        q : "How do you start a conditional statement?",
-        a: ["else","else if","if","else if"],
-        rightAnswer : 2
-    },
-    questionFour = {
-        q : "what special character do you use to declare an array",
-        a: ["{}","[]","<>","$$"],
-        rightAnswer : 1
-    },
-    questionFive = {
-        q : "how do you set the value of a variable?",
-        a: ["==","===","=","!="],
-        rightAnswer : 2
-    },
-    questionSix = {
-        q : "What tag we use to link a JavaScript file to html",
-        a: ["js","javascript","link","script"],
-        rightAnswer : 3
-    },
-    questionSeven = {
-        q : "whats the correct syntax for a for loop?",
-        a: ["for i = 0, i < 5, i++",
-            "(for i = 0; i < 5; i++)",
-            "for i < 5",
-            "for i to 5"],
-        rightAnswer : 1
-    },
-    questionEight = {
-        q : "What keyword do you use to return value from a function",
-        a: ["return","break","float","string"],
-        rightAnswer : 0
-    },
-    questionNine = {
-        q : "What event is use to listed when the user, uses enter to input value ",
-        a: ["click","hover","dblclick","submit"],
-        rightAnswer : 3
-    },
-    questionTen = {
-        q : "How do you target an elemnt by ID",
-        a: ["document.getElementByClass()","document.getElementById()","document.querySelector()","document.getElementsByTagName"],
-        rightAnswer : 1
+// high score
+const highscoreEl = document.querySelector('#highscores');
+const usernamesEl = document.querySelector('#usernames');
+const scoresEl = document.querySelector('#scores');
+
+// Timer/Score Variables
+let timeLeft = 60;
+const timerDisplayEL = document.getElementById('timer');
+let interval;
+
+// button actions
+
+// start quiz
+startBtnEl.addEventListener('click', startQuiz)
+
+// restart quiz
+retakeBtnEl.addEventListener('click', reloadTest)
+
+// Submit score button
+submitBtnEl.addEventListener('click', submitScore)
+viewHighscoresEl.addEventListener('click', () => {
+    startScreenEl.classList.add('hide');
+    questionContainerEl.classList.add('hide');
+    gameOverEl.classList.add('hide');
+    highscoreEl.classList.remove('hide');
+})
+
+
+// Functions
+function timeHandler() {
+    interval = setInterval(function() {
+
+        // validate that there is still time left for playing
+        if (timeLeft <= 0){
+            clearInterval(timeLeft = 0);
+            // Alert user to the end of the game
+            startScreenEl.classList.add('hide');
+            questionContainerEl.classList.add('hide')
+            gameOverEl.classList.remove('hide')
+        };
+
+        // Render time for display
+        timerDisplayEL.innerHTML = ('Timer/Score: ' + timeLeft)
+
+        timeLeft -= 1
+    }, 1000)     
+};
+
+function startQuiz() {
+
+    startScreenEl.classList.add('hide');
+    questionContainerEl.classList.remove('hide');
+    gameOverEl.classList.add('hide');
+    timeHandler();
+    questionIndex = 0;
+    randomQuestion = questions.sort(() => Math.random() - .5);
+    showNewQuestion(randomQuestion, questionIndex);
+};
+
+function showNewQuestion(randomQuestion, questionIndex) {
+    if (questionIndex >= randomQuestion.length) {
+        clearInterval(interval)
+        questionContainerEl.classList.add('hide');
+        gameOverEl.classList.remove('hide')
+        return;
     }
-]
+    let currentQuestion = randomQuestion[questionIndex];
+    questionEl.innerText = currentQuestion.question
 
-function startGame(event){
-    score = 0;
+    answers.forEach(answer => {
+        const number = answer.dataset['number'];
+        answer.innerText = number + '. ' + currentQuestion['answer' + number];
+    })
+};
 
-    event.preventDefault();
-    clearInterval(interval);
+// FOR EACH answer button 
+answers.forEach(answer => {
+    // If the user CLICKS on a button we want certain actions to happen.
+    answer.addEventListener('click', event => {
+        event.preventDefault();
+        const selectedAnswer = event.target.dataset.number
 
-    interval = setInterval(Timer, 1000);
-    introBlock.style.display = "none";
-    questionSection.style.display = "block"; 
+    // IF the button they click on is the right answer
+    if (selectedAnswer == questions[questionIndex].correctAnswer) {
+
+        questionIndex++;
+        showNewQuestion(randomQuestion, questionIndex);
+    }
+
+    // wrong answer function
+    else if (selectedAnswer != questions[questionIndex].correctAnswer) {
+        timeLeft-=10;
+        questionIndex++;
+        showNewQuestion(randomQuestion, questionIndex);
+    }
+    })
     
-    displayQuestion(); 
-}
+})
 
-//sets time and calls display time to display time
-function timeHandler(){ 
-    
-    //check every time to see if time has ran out
-    if (totalSeconds <= 0){
-        finalScore();
-    }
-    totalSeconds--;
-    var totalMinutes = Math.floor(totalSeconds/ 60);
-    minutesLeft = totalMinutes;
-    //console.log(totalMinutes);
-    var secondsPerMinute = totalSeconds % 60;
-    displayTime(totalMinutes, secondsPerMinute);
-   // console.log(totalMinutes);
-}
+function submitScore() {
+    let score = timeLeft;
+    let userScore = {
+        User: userIdEL, 
+        Score: score
+    };
+    let userScoreString = JSON.stringify(userScore);
+    localStorage.setItem("userScore", userScoreString);
+    console.log(localStorage);
+    highscoreEl.classList.remove('hide');
+    gameOverEl.classList.add('hide');
+};
 
-//formats time as displays time
-function displayTime(totalMinutes, secondsPerMinute){
-   // console.log(totalMinutes, secondPerMinute);
-    if(totalMinutes == 2){
-        totalMinutes = 1;
-    }
-
-    if(secondsPerMinute < 10){
-        timeLeft.textContent = totalMinutes + ":" + "0" + secondsPerMinute;
-    }
-    else{
-        timeLeft.textContent = totalMinutes + ":" + secondsPerMinute;
-    }
-    secondsLeft = secondsPerMinute; 
-}
-
-//creates four children for each answer in every question and display the questions
-function displayQuestion(){
-    
-    questionEl.textContent = questions[currentQuestion].q;
-    for(i =0; i < questions[currentQuestion].a.length; i++){
-        var li = document.createElement("li")
-        li.innerHTML = "<button>" + questions[currentQuestion].a[i] +"</button>";
-        li.dataIndex = i;
-        console.log(li.dataIndex );
-        answers.append(li);
-    }
-     
-}
-
-//when you click each button is going to call check answer function to check answers
-function answersButtons(event){
-    if(event.target.matches("button")){
-        event.preventDefault(); 
-        //everytime a button is clicked check if they reached the end of questions
-        if (currentQuestion === questions.length-1){
-            finalScore();
-        }
-        //console.log(e.target.parentElement.dataIndex);
-        var index = parseInt(event.target.parentElement.dataIndex)
-        // console.log(index);
-        checkAnswer(index);
-        removeChildren(answers);
-        currentQuestion++;
-        displayQuestion();
-        
-    }
-}
-
-//check answer to check right answer al;so displayus if correct
-function checkAnswer(index){
-    if(index === questions[currentQuestion].rightAnswer){
-        correctOrWrongEl.style.opacity = 1;
-        correctOrWrongAnswer.textContent = "Corret";
-        correctSound.play();
-        score++; 
-    }
-    else{
-        correctOrWrongEl.style.opacity = 1;
-        correctOrWrongAnswer.textContent = "Wrong";
-        wrongSound.play(); 
-
-        totalSeconds = totalSeconds - 20;
-        //console.log(totalSeconds);
-        if (totalSeconds <= 0){
-            finalScore();
-        }
-    } 
-    //console.log(score);
-  
-    window.setTimeout("correctOrWrongEl.style.opacity = 0;", 500);
-}
-
-//removes children from parent element 
-function removeChildren(parent){
-    while(parent.firstChild){
-        parent.removeChild(parent.firstChild);
-    }
-}
-
-//clears interval and send to all done block
-function finalScore(){
-    playAgainClearTime();
-    totalScore.textContent = "Your Score: " + score;
-    questionSection.style.display = "none";
-    scoreInfo.style.display = "block";
-}
-
-///clears interval and sets everything to starting mode
-function playAgainClearTime(){
-    currentQuestion = 0;
-    totalSeconds = 120;
-    clearInterval(interval);
-}
-
-//click and submit event, will change to highscore Html
-//get and sets player info so it can be used as highscores list
-function submitScore(event){
-    event.preventDefault();
-    scoreInfo.style.display = "none";
-    var intials = intialsEl.value;
-    var playerList = [];
-    //console.log(JSON.parse(localStorage.getItem("array")));
-    if(JSON.parse(localStorage.getItem("array")) === undefined){
-        playerList =[{name : intials, score: score, time: timeLeft.textContent }];
-    }
-    else{
-        playerList = JSON.parse(localStorage.getItem("array"));
-    }
-
-    playerList.push({name : intials, score: score, time: timeLeft.textContent});
-    localStorage.setItem("array", JSON.stringify(playerList));
-    //console.log(playerList);
-    window.location.href = "highscores.html";
-}
-
-function createScoreListitem(){
-    var storedArray = JSON.parse(localStorage.getItem("array"));
-    //console.log(storedArray);
-    //removeChildren(highscoresList);
-    for(i = 0; i < storedArray.length; i++){
-        var li = document.createElement("li");
-        li.innerHTML =  "<span>Initials:</span> "+ storedArray[i].name + "  <span>Score:</span>" + storedArray[i].score + " <span>Time:</span>" + storedArray[i].time;
-        listHighscores.append(li);
-    }
-}
-
-function clearFunction(event){
-    event.preventDefault();
-    localStorage.setItem("array", "[]");
-    //console.log(playerList = JSON.parse(localStorage.getItem("array", "[]")));
-    removeChildren(listHighscores);
-}
-
-function goBack(event){
-    event.preventDefault();
-   playAgainClearTime();
-   location.href = "index.html";
+function reloadTest() {
+    location.reload();
+    return;
 }
 
 
-clearButton.addEventListener("click", clearFunction);
-backButtonEl.addEventListener("click", goBack);
-
-createScoreListitem();
-
-buttonStart.addEventListener("click",startGame);
-answers.addEventListener("click", answersButtons);
-submitBtn.addEventListener("click", submitScore);
-intialsEl.addEventListener("submit",submitScore);
